15f3963524047078c83613e45d6badb28261092e
kon_k <kon_kostas@windowslive.com>
2022-02-03 21:17:01 +0200

    add Cross Origin support to HelloController

--- src/main/java/gr/konkos/FourHasFourLetters/FourHasFourLettersApplication.java
+++ src/main/java/gr/konkos/FourHasFourLetters/FourHasFourLettersApplication.java
@@ -2,6 +2,7 @@ package gr.konkos.FourHasFourLetters;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +14,8 @@ public class FourHasFourLettersApplication {
 	}
 
 	@RestController
-	class HelloController{
+	@CrossOrigin
+	static class HelloController{
 		@GetMapping("/")
 		public String sayHello(){
 			return "Hello";
